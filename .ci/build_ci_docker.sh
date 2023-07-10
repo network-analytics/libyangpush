9749526b6204aa922be6e866fa32d02f4f7edccd
Thomas Joubert 50022150 <thomas.joubert@huawei.com>
2023-07-10 15:10:28 +0100

    feat(ci): create whole custom image and use it in gitlab-ci
    Signed-off-by: Thomas Joubert 50022150 <thomas.joubert@huawei.com>

---
+++ .ci/build_ci_docker.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker build --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg HTTPS_PROXY=$HTTPS_PROXY --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t libyangpush-ci .
