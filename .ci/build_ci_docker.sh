7aa030966366adb554631be62f6e229ae457c2e6
Thomas Joubert 50022150 <thomas.joubert@huawei.com>
2023-07-11 15:15:56 +0100

    feat(ci): update docker image to bypass docker & gitlab-runner bug
    Signed-off-by: Thomas Joubert 50022150 <thomas.joubert@huawei.com>

--- .ci/build_ci_docker.sh
+++ .ci/build_ci_docker.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker build --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg HTTPS_PROXY=$HTTPS_PROXY --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t registry-eu.inhuawei.com:5050/libyangpush-ci:v1.0.1 .
+docker build --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg HTTPS_PROXY=$HTTPS_PROXY --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t registry-eu.inhuawei.com:5050/libyangpush-ci:v1.1.0 .
