1a733d8819aa29d2a2725766cf22c960a9568911
z50031928 <zhuoyao.lin@huawei.com>
2023-07-14 20:34:14 +0800

    feat(build): Add compile flag for meson.build

--- src/tool.c
+++ src/tool.c
@@ -5,7 +5,7 @@ unsigned long djb2(char *str)
     unsigned long hash = 5381;
     int c;
 
-    while (c = *str++) {
+    while ((c = *str++)) {
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
     }
 
