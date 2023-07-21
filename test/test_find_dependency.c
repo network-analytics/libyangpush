9e6840a7ad929362ffd55047e02c78f4818bdffc
z50031928 <zhuoyao.lin@huawei.com>
2023-07-21 21:45:10 +0800

    feat(test_dep):support assert_found_module(0

--- test/test_find_dependency.c
+++ test/test_find_dependency.c
@@ -26,6 +26,16 @@ char* load_yang_example(char *filename)
     return text;
 }
 
+/* Check the correctness of module stored in 'module_set'*/
+void assert_found_module(char *module_name, char *yang_code, cdada_map_t *module_set){
+    unsigned long hash = djb2(module_name);
+    struct module_info *module_ptr = NULL;
+    assert_int_equal(cdada_map_find(module_set, &hash, (void**)&module_ptr), CDADA_SUCCESS); // If module is inthe map or not
+    assert_string_equal(module_ptr->yang_code, yang_code);
+    assert_string_equal(module_ptr->name, module_name);
+    return;
+}
+
 static void test_load_module_to_map(void** state)
 {
     struct ly_ctx *test_ctx;
@@ -38,18 +48,15 @@ static void test_load_module_to_map(void** state)
     char *amodule_text = load_yang_example("../test/resources/load_module_test/a-module.yang");
 
     struct lys_module* test_amodule = ly_ctx_load_module(test_ctx, "a-module", NULL, NULL);
-    unsigned long hash_of_amodule = djb2("a-module"), hash_of_cmodule = djb2("c-module");
-    struct module_info *amodule_info = NULL, *cmodule_info = NULL;
     cdada_map_t* test_map = cdada_map_create(256);
 
     // Test 1: Valid test case. Load a-module and e-module in the map.
     assert_int_equal(libyangpush_load_module_into_map(test_map, test_amodule), INSERT_SUCCESS);
     assert_int_equal(libyangpush_load_submodule_into_map(test_map, test_amodule->parsed->includes->submodule), INSERT_SUCCESS);
+
     assert_int_equal(cdada_map_size(test_map), 2);
-    assert_int_equal(cdada_map_find(test_map, &hash_of_amodule, (void**)&amodule_info), CDADA_SUCCESS);
-    assert_int_equal(cdada_map_find(test_map, &hash_of_cmodule, (void**)&cmodule_info), CDADA_SUCCESS);
-    assert_string_equal(amodule_info->yang_code, amodule_text);
-    assert_string_equal(cmodule_info->yang_code, cmodule_text);
+    assert_found_module("a-module", amodule_text, test_map);
+    assert_found_module("c-module", cmodule_text, test_map);
 
     // Test 2: Non-valid test case. Passed null pointer into function
     assert_int_equal(libyangpush_load_module_into_map(NULL, NULL), INVALID_PARAMETER);
@@ -83,21 +90,13 @@ static void test_find_import(void** state)
     ly_ctx_load_module(test1_ctx, "e-module", NULL, NULL);
     
     cdada_map_t* test1_module_set = cdada_map_create(256);
-    unsigned long hash_of_bmodule = djb2("b-module"), hash_of_cmodule = djb2("c-module"), hash_of_emodule = djb2("e-module");
-    struct module_info *bmodule_ptr = NULL, *cmodule_ptr = NULL, *emodule_ptr = NULL;
 
     //Test1: A valid test case. Find the import for a-module and check if it has been put into the cdada map
     assert_int_equal(libyangpush_find_import(test1_amodule->parsed->imports, test1_module_set), FIND_DEPENDENCY_SUCCESS);
     assert_int_equal(cdada_map_size(test1_module_set), 3); //num of modules in the map
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_bmodule, (void**)&bmodule_ptr), CDADA_SUCCESS); // Check b-module
-    assert_string_equal(bmodule_ptr->yang_code, test1_bmodule_text);
-    assert_string_equal(bmodule_ptr->name, "b-module");
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_cmodule, (void**)&cmodule_ptr), CDADA_SUCCESS); // Check c-module
-    assert_string_equal(cmodule_ptr->yang_code, test1_cmodule_text);
-    assert_string_equal(cmodule_ptr->name, "c-module");
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_emodule, (void**)&emodule_ptr), CDADA_SUCCESS); // Check e-module
-    assert_string_equal(emodule_ptr->yang_code, test1_emodule_text);
-    assert_string_equal(emodule_ptr->name, "e-module");
+    assert_found_module("b-module", test1_bmodule_text, test1_module_set);
+    assert_found_module("c-module", test1_cmodule_text, test1_module_set);
+    assert_found_module("e-module", test1_emodule_text, test1_module_set);
 
     //load b-module in scenario 2 into test2 context
     struct lys_module* test2_bmodule = ly_ctx_load_module(test2_ctx, "b-module", NULL, NULL);
@@ -137,21 +136,13 @@ static void test_find_include(void** state)
     ly_ctx_load_module(test1_ctx, "e-module", NULL, NULL);
 
     cdada_map_t* test1_module_set = cdada_map_create(256);
-    unsigned long hash_of_cmodule = djb2("c-module"), hash_of_ccmodule = djb2("cc-module"), hash_of_emodule = djb2("e-module");
-    struct module_info *cmodule_ptr = NULL, *ccmodule_ptr = NULL, *emodule_ptr = NULL;
 
     //Test1: A valid test case. Find the include for a-module and check if it has been put into the cdada map
     assert_int_equal(libyangpush_find_include(test1_amodule->parsed->includes, test1_module_set), FIND_DEPENDENCY_SUCCESS);
     assert_int_equal(cdada_map_size(test1_module_set), 3);
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_cmodule, (void**)&cmodule_ptr), CDADA_SUCCESS); //for c-module
-    assert_string_equal(cmodule_ptr->yang_code, test1_cmodule_text);
-    assert_string_equal(cmodule_ptr->name, "c-module");
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_ccmodule, (void**)&ccmodule_ptr), CDADA_SUCCESS); //for cc-module
-    assert_string_equal(ccmodule_ptr->yang_code, test1_ccmodule_text);
-    assert_string_equal(ccmodule_ptr->name, "cc-module");
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_emodule, (void**)&emodule_ptr), CDADA_SUCCESS); //for e-module
-    assert_string_equal(emodule_ptr->yang_code, test1_emodule_text);
-    assert_string_equal(emodule_ptr->name, "e-module");
+    assert_found_module("c-module", test1_cmodule_text, test1_module_set);
+    assert_found_module("cc-module", test1_ccmodule_text, test1_module_set);
+    assert_found_module("e-module", test1_emodule_text, test1_module_set);
 
     //load b-module in scenario 2 into test2 context
     struct lys_module* test2_bmodule = ly_ctx_load_module(test2_ctx, "b-module", NULL, NULL);
@@ -195,23 +186,15 @@ static void test_find_reverse_dep(void** state)
     ly_ctx_load_module(test1_ctx, "e-module", NULL, NULL);
     
     cdada_map_t* test1_module_set = cdada_map_create(256);
-    unsigned long hash_of_dmodule = djb2("d-module"), hash_of_cmodule = djb2("c-module"), hash_of_emodule = djb2("e-module");
-    struct module_info *dmodule_ptr = NULL, *cmodule_ptr = NULL, *emodule_ptr = NULL;
     libyangpush_load_module_into_map(test1_module_set, test1_amodule); /* parent module needs to be inserted into map to avoid
                                                                           their reverse dependency identifying them as an import*/
 
     //Test1: A valid test case for finding augment. Find the augment for a-module and check if it has been put into the cdada map
     assert_int_equal(libyangpush_find_reverse_dep(test1_amodule->augmented_by, test1_module_set), FIND_DEPENDENCY_SUCCESS);
     assert_int_equal(cdada_map_size(test1_module_set), 4);
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_dmodule, (void**)&dmodule_ptr), CDADA_SUCCESS);
-    assert_string_equal(dmodule_ptr->yang_code, test1_dmodule_text);//for d-module
-    assert_string_equal(dmodule_ptr->name, "d-module");
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_cmodule, (void**)&cmodule_ptr), CDADA_SUCCESS);
-    assert_string_equal(cmodule_ptr->yang_code, test1_cmodule_text);//for c-module
-    assert_string_equal(cmodule_ptr->name, "c-module");
-    assert_int_equal(cdada_map_find(test1_module_set, &hash_of_emodule, (void**)&emodule_ptr), CDADA_SUCCESS);
-    assert_string_equal(emodule_ptr->yang_code, test1_emodule_text);//for e-module
-    assert_string_equal(emodule_ptr->name, "e-module");
+    assert_found_module("d-module", test1_dmodule_text, test1_module_set);
+    assert_found_module("c-module", test1_cmodule_text, test1_module_set);
+    assert_found_module("e-module", test1_emodule_text, test1_module_set);
 
     //load b-module in scenario 2 into test2 context
     struct lys_module* test2_bmodule = ly_ctx_load_module(test2_ctx, "b-module", NULL, NULL);
@@ -227,23 +210,15 @@ static void test_find_reverse_dep(void** state)
     ly_ctx_load_module(test3_ctx, "a-module-deviations", NULL, NULL);
 
     cdada_map_t* test3_module_set = cdada_map_create(256);
-    unsigned long hash_of_deviate = djb2("a-module-deviations"), hash_of_strmodule = djb2("str-type");
-    struct module_info *deviate_cmodule_ptr = NULL, *deviate_strmodule_ptr = NULL, *deviate_amd_ptr = NULL;
     libyangpush_load_module_into_map(test3_module_set, test3_amodule); /* parent module needs to be inserted into map to avoid
                                                                           their reverse dependency identifying them as an import*/
 
     //Test3: A valid test case for finding deviate. Find the deviations for a-module and check if it has been put into the cdada map
     assert_int_equal(libyangpush_find_reverse_dep(test3_amodule->deviated_by, test3_module_set), FIND_DEPENDENCY_SUCCESS);
     assert_int_equal(cdada_map_size(test3_module_set), 4);
-    assert_int_equal(cdada_map_find(test3_module_set, &hash_of_deviate, (void**)&deviate_amd_ptr), CDADA_SUCCESS);
-    assert_string_equal(deviate_amd_ptr->yang_code, test3_deviate_text);
-    assert_string_equal(deviate_amd_ptr->name, "a-module-deviations");
-    assert_int_equal(cdada_map_find(test3_module_set, &hash_of_strmodule, (void**)&deviate_strmodule_ptr), CDADA_SUCCESS);
-    assert_string_equal(deviate_strmodule_ptr->yang_code, test3_strmodule_text);
-    assert_string_equal(deviate_strmodule_ptr->name, "str-type");
-    assert_int_equal(cdada_map_find(test3_module_set, &hash_of_cmodule, (void**)&deviate_cmodule_ptr), CDADA_SUCCESS);
-    assert_string_equal(deviate_cmodule_ptr->yang_code, test3_cmodule_text);
-    assert_string_equal(deviate_cmodule_ptr->name, "c-module");
+    assert_found_module("a-module-deviations", test3_deviate_text, test3_module_set);
+    assert_found_module("str-type", test3_strmodule_text, test3_module_set);
+    assert_found_module("c-module", test3_cmodule_text, test3_module_set);
 
     cdada_map_traverse(test1_module_set, &libyangpush_trav_clear_map, NULL);
     cdada_map_traverse(test3_module_set, &libyangpush_trav_clear_map, NULL);
